1fe3de78be9f89d1d7f65c7cebb26f3b20f81bc5
Dmytro Denysov <dmytro.denysov@danads.se>
2021-11-23 22:53:31 +0200

    Added telegram bot bundle

--- src/Command/SendScheduleCommand.php
+++ src/Command/SendScheduleCommand.php
@@ -2,31 +2,33 @@
 
 namespace App\Command;
 
-use Longman\TelegramBot\Exception\TelegramException;
-use Longman\TelegramBot\Telegram;
+use Borsaco\TelegramBotApiBundle\Service\Bot;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SendScheduleCommand extends Command
 {
+
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:send-schedule';
-
     /**
-     * Configure
+     * @var \Borsaco\TelegramBotApiBundle\Service\Bot
      */
-    protected function configure(): void
+    private $bot;
+
+    public function __construct(Bot $bot, string $name = null)
     {
-        // ...
+        parent::__construct($name);
+        $this->bot = $bot;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $message = urlencode('Todays number: ' . rand(1, 99999999));
-        $url = "https://api.telegram.org/bot2118172018:AAE3BGPeOkqTw3624TSj9kMi-Y-o40FnkxI/sendMessage?text=$message&chat_id=392059332";
-
-        file_get_contents($url);
+        $this->bot->getBot('first')->sendMessage([
+            'chat_id' => '392059332',
+            'text'    => 'Todays number: ' . rand(1, 99999999),
+        ]);
 
         $output->writeln('Success');
 
