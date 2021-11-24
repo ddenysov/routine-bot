4ee535a8b1836d6ee43dc30d414cc9453350ac6b
Dmytro Denysov <dmytro.denysov@danads.se>
2021-11-24 16:30:07 +0200

    Initial commit

--- src/Command/SendScheduleCommand.php
+++ src/Command/SendScheduleCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use Borsaco\TelegramBotApiBundle\Service\Bot;
+use Google_Service_Sheets;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,11 +24,31 @@ class SendScheduleCommand extends Command
         $this->bot = $bot;
     }
 
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException|\Google\Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $key    = json_decode(file_get_contents('/var/www/html/var/client_secret.json'), true);
+        $sheet  = json_decode(file_get_contents('/var/www/html/var/sheet.json'), true);
+        $client = new \Google_Client($key);
+
+        $client->setApplicationName('Google Sheets and PHP');
+        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+
+        $service       = new Google_Service_Sheets($client);
+        $spreadsheetId = $sheet['sheet'];
+        $range         = "Tasks!A:E";
+
+        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $values = $response->getValues();
+
         $this->bot->getBot('first')->sendMessage([
             'chat_id' => '392059332',
-            'text'    => 'Todays number: ' . rand(1, 99999999),
+            'text'    => $values[rand(1, 4)][3] . ' - ' . $values[rand(1, 4)][0],
         ]);
 
         $output->writeln('Success');
--- src/Controller/WebHookController.php
+++ src/Controller/WebHookController.php
@@ -2,10 +2,6 @@
 
 namespace App\Controller;
 
-
-use Longman\TelegramBot\Exception\TelegramException;
-use Longman\TelegramBot\Telegram;
-
 class WebHookController
 {
     /**
@@ -13,19 +9,6 @@ class WebHookController
      */
     public function index()
     {
-        $bot_api_key  = '2118172018:AAE3BGPeOkqTw3624TSj9kMi-Y-o40FnkxI';
-        $bot_username = 'ATLAS_DAILY_BOT';
-
-        try {
-            // Create Telegram API object
-            $telegram = new Telegram($bot_api_key, $bot_username);
-
-            // Handle telegram webhook request
-            $telegram->handle();
-        } catch (TelegramException $e) {
-            // Silence is golden!
-            // log telegram errors
-            // echo $e->getMessage();
-        }
+        die('What have you expected to see here?');
     }
 }
